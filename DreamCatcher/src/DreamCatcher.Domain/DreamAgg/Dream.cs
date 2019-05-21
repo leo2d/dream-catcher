68baab7d1d47a0b070000164066fb83c4a5f6ecf
Leonardo Dias <leo-rock14@hotmail.com>
2019-05-21 20:26:02 -0300

    criacao dos modelos iniciais e correcoes de bugs

--- DreamCatcher/src/DreamCatcher.Domain/DreamAgg/Dream.cs
+++ DreamCatcher/src/DreamCatcher.Domain/DreamAgg/Dream.cs
@@ -1,8 +1,18 @@
-﻿using DreamCatcher.Domain.SharedKernel.Entities;
+﻿using DreamCatcher.Domain.DreamAgg.Entities;
+using DreamCatcher.Domain.SharedKernel.Entities;
+using DreamCatcher.Domain.UserAgg;
+using System;
+using System.Collections.Generic;
 
 namespace DreamCatcher.Domain.DreamAgg
 {
     public class Dream : BaseEntity<Dream>
     {
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+
+        public Guid IDUser { get; set; }
+        public User User { get; set; }
+        public IEnumerable<DreamTask> Tasks { get; set; }
     }
 }
---
+++ DreamCatcher/src/DreamCatcher.Domain/DreamAgg/Entities/DreamTask.cs
@@ -0,0 +1,14 @@
+﻿using DreamCatcher.Domain.SharedKernel.Entities;
+using System;
+
+namespace DreamCatcher.Domain.DreamAgg.Entities
+{
+    public class DreamTask : BaseEntity<DreamTask>
+    {
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+        public Dream Dream { get; set; }
+
+        public Guid IdDream { get; set; }
+    }
+}
